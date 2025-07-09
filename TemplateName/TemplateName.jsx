4862ae28f2d879d4607aa597089408fd60b52b9b
Shahd Elsherif <Shahd.Elsherif@linkdev.com>
2025-07-09 12:50:27 +0300

    remove second

--- TemplateName/TemplateName.jsx
+++ TemplateName/TemplateName.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 export default function TemplateName() {
     
-    const [first, setfirst] = useState(second)
+    const [first, setfirst] = useState(0)
 
     useEffect(() => {
       first
--- src/compomemts/NotFound/NotFound.jsx
+++ src/compomemts/NotFound/NotFound.jsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react'
 
 export default function NotFound() {
     
-    const [first, setfirst] = useState(second)
-
     useEffect(() => {
       first
     
